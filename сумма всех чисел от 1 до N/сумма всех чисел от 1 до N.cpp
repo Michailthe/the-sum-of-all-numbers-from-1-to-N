0035e888b180585926bc3d4d2fb37272282d95d8
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-20 00:07:34 +0300

    Добавьте файлы проекта.

---
+++ сумма всех чисел от 1 до N/сумма всех чисел от 1 до N.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main() {
+    int N, sum = 0;
+
+    std::cout << "Введите число N: ";
+    std::cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+
+    std::cout << "Сумма всех чисел от 1 до " << N << ", которые делятся на 3 или 5: " << sum << std::endl;
+
+    return 0;
+}
